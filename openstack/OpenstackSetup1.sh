77acd06b5eab307e0c5b9f57d0d058ad71f70fce
ethender <ethender@hotmail.com>
2019-03-24 15:12:39 +0530

    Bug Fixies

--- openstack/OpenstackSetup1.sh
+++ openstack/OpenstackSetup1.sh
@@ -1,51 +1,51 @@
 #!/bin/sh
 touch /var/log/openstack_automate.log
 LOG_FILE='/var/log/openstack_automate.log'
-echo 'Openstack pre operations has started' >> LOG_FILE
-echo '#######################################' >> LOG_FILE
+echo 'Openstack pre operations has started' >> $LOG_FILE
+echo '#######################################' >> $LOG_FILE
 # Red hat release
-echo 'Red hat release' >> LOG_FILE
-cat /etc/redhat-release
+echo 'Red hat release' >> $LOG_FILE
+cat /etc/redhat-release >> $LOG_FILE
 
 # setting environment
 echo '##################################'
-mv /etc/environment /etc/environment.orig >> LOG_FILE
-cp environment /etc/environment >> LOG_FILE
-cat /etc/environment >> LOG_FILE
+mv /etc/environment /etc/environment.orig >> $LOG_FILE
+cp environment /etc/environment >> $LOG_FILE
+cat /etc/environment >> $LOG_FILE
 
 # check and disabling firewall
 echo '##################################'
-systemctl status firewalld >> LOG_FILE
-systemctl stop firewalld >> LOG_FILE
-systemctl disable firewalld >> LOG_FILE
-echo 'Firewalld has stopped' >> LOG_FILE
+systemctl status firewalld >> $LOG_FILE
+systemctl stop firewalld >> $LOG_FILE
+systemctl disable firewalld >> $LOG_FILE
+echo 'Firewalld has stopped' >> $LOG_FILE
 
 # check and disabling network manager
-echo '##################################' >> LOG_FILE
-systemctl status NetworkManager >> LOG_FILE
-systemctl stop NetworkManager >> LOG_FILE
-systemctl disable NetworkManager >> LOG_FILE
-echo 'Network Manager has changed' >> LOG_FILE
+echo '##################################' >> $LOG_FILE
+systemctl status NetworkManager >> $LOG_FILE
+systemctl stop NetworkManager >> $LOG_FILE
+systemctl disable NetworkManager >> $LOG_FILE
+echo 'Network Manager has changed' >> $LOG_FILE
 
 #enabling Network
-echo '####################################' >>  LOG_FILE
-systemctl enable network >>  LOG_FILE
-systemctl start network >>  LOG_FILE
-echo 'Network has started' >> LOG_FILE
+echo '####################################' >>  $LOG_FILE
+systemctl enable network >>  $LOG_FILE
+systemctl start network >>  $LOG_FILE
+echo 'Network has started' >> $LOG_FILE
 
 #copying network settings
-echo '####################################' >> LOG_FILE
-echo 'Enter Your Bridge Name ex - enp0s3' >> LOG_FILE
-read -p "Enter network bridge name: " bridge 
-cat /etc/sysconfig/network-scripts/ifcfg-$bridge >> LOG_FILE
-mkdir /tempconfig >> LOG_FILE
-cp /etc/sysconfig/network-scripts/ifcfg-$bridge /tempconfig >> LOG_FILE
-echo 'network backup settings has done. /tempconfig' >> LOG_FILE
+echo '####################################' >> $LOG_FILE
+echo 'Enter Your Bridge Name ex - enp0s3' >> $LOG_FILE
+read -p "Enter network bridge name: " bridge
+cat /etc/sysconfig/network-scripts/ifcfg-$bridge >> $LOG_FILE
+mkdir /tempconfig >> $LOG_FILE
+cp /etc/sysconfig/network-scripts/ifcfg-$bridge /tempconfig >> $LOG_FILE
+echo 'network backup settings has done. /tempconfig' >> $LOG_FILE
 
 #se linux should be disabled
-echo '####################################' >> LOG_FILE
-mv /etc/selinux/config  /etc/selinux/config.orig >> LOG_FILE
-cp seconfig /etc/selinux/ >> LOG_FILE
-echo 'se linux has been modified' >> LOG_FILE
-echo 'Please reboot the system.' >> LOG_FILE
-echo 'After rebooting. Execute Openstack setup 2' >> LOG_FILE
+echo '####################################' >> $LOG_FILE
+mv /etc/selinux/config  /etc/selinux/config.orig >> $LOG_FILE
+cp seconfig /etc/selinux/ >> $LOG_FILE
+echo 'se linux has been modified' >> $LOG_FILE
+echo 'Please reboot the system.' >> $LOG_FILE
+echo 'After rebooting. Execute Openstack setup 2' >> $LOG_FILE
--- openstack/OpenstackSetup2.sh
+++ openstack/OpenstackSetup2.sh
@@ -3,46 +3,46 @@ LOG_FILE='/var/log/openstack_automate.log'
 #
 # After reboot setup2
 #
-echo '#####################################' >> LOG_FILE
-echo 'Openstack setup 2 has started' >> LOG_FILE
-getenforce >> LOG_FILE
+echo '#####################################' >> $LOG_FILE
+echo 'Openstack setup 2 has started' >> $LOG_FILE
+getenforce >> $LOG_FILE
 
 #installing openstack package
 echo  '#######################################'
-yum install -y centos-release-openstack-ocata >> LOG_FILE
-echo 'opentstack octa has installed' >> LOG_FILE
-echo 'updating opentack system' >> LOG_FILE
-yum update -y >> LOG_FILE
+yum install -y centos-release-openstack-ocata >> $LOG_FILE
+echo 'opentstack octa has installed' >> $LOG_FILE
+echo 'updating opentack system' >> $LOG_FILE
+yum update -y >> $LOG_FILE
 
 #installing packstack
-echo  '#######################################' >> LOG_FILE
-echo 'It Will take n1-2 hours.' >> LOG_FILE
-echo 'Have some patience.' >> LOG_FILE
-echo 'After installation. Read the CLI_installation_guide.pdf file.' >> LOG_FILE
-echo 'After installing check internet connectivity is there.' >> LOG_FILE
-echo 'After modifiying the connectivity. Is any problem check /tempconfig for network configuration has backup' >> LOG_FILE
+echo  '#######################################' >> $LOG_FILE
+echo 'It Will take n1-2 hours.' >> $LOG_FILE
+echo 'Have some patience.' >> $LOG_FILE
+echo 'After installation. Read the CLI_installation_guide.pdf file.' >> $LOG_FILE
+echo 'After installing check internet connectivity is there.' >> $LOG_FILE
+echo 'After modifiying the connectivity. Is any problem check /tempconfig for network configuration has backup' >> $LOG_FILE
 
-yum install -y openstack-packstack >> LOG_FILE
+yum install -y openstack-packstack >> $LOG_FILE
 
-echo '###########################################' >> LOG_FILE
-echo 'Ip Address Review' >> LOG_FILE
-ip address show >> LOG_FILE
-
-
-echo '#############################################' >> LOG_FILE
-echo 'Installing Openstack setup' >> LOG_FILE
-packstack --allinone --provision-demo=n --os-neutron-ovs-bridge-mappings=extnet:br-ex --os-neutron-ml2-type-drivers=vxlan,flat >> LOG_FILE
+echo '###########################################' >> $LOG_FILE
+echo 'Ip Address Review' >> $LOG_FILE
+ip address show >> $LOG_FILE
 
 
-echo '############################################' >> LOG_FILE
-echo 'Network Setup started' >> LOG_FILE
-echo 'Enter Your Bridge Name ex - enp0s3' >> LOG_FILE
+echo '#############################################' >> $LOG_FILE
+echo 'Installing Openstack setup' >> $LOG_FILE
+packstack --allinone --provision-demo=n --os-neutron-ovs-bridge-mappings=extnet:br-ex --os-neutron-ml2-type-drivers=vxlan,flat >> $LOG_FILE
+
+
+echo '############################################' >> $LOG_FILE
+echo 'Network Setup started' >> $LOG_FILE
+echo 'Enter Your Bridge Name ex - enp0s3' >> $LOG_FILE
 read -p "Enter network bridge name: " bridge
-echo 'your network ip address ex- 192.168.1.200' >> LOG_FILE
+echo 'your network ip address ex- 192.168.1.200' >> $LOG_FILE
 read -p "Enter network IP Address : " ip
-echo 'your network netmask ex- 255.255.255.0' >> LOG_FILE
+echo 'your network netmask ex- 255.255.255.0' >> $LOG_FILE
 read -p "Enter Netmask:" netmask
-echo 'your network gateway ex- 192.168.1.1' >> LOG_FILE
+echo 'your network gateway ex- 192.168.1.1' >> $LOG_FILE
 read -p "GATEWAY: "gateway
 sudo touch /etc/sysconfig/network-scripts/ifcfg-br-ex
 br-ex=/etc/sysconfig/network-scripts/ifcfg-br-ex
@@ -57,12 +57,12 @@ sudo $br-ex >> 'IPV6INIT=no'
 sudo $br-ex >> 'DNS1=8.8.8.8'
 sudo $br-ex >> 'ONBOOT=yes'
 
-echo "###############################################" >> LOG_FILE
+echo "###############################################" >> $LOG_FILE
 echo 'Changed br-ex file'
-cat $br-ex >> LOG_FILE
+cat $br-ex >> $LOG_FILE
 
-echo '###############################################' >> LOG_FILE
-echo 'Changing bridge file' >> LOG_FILE
+echo '###############################################' >> $LOG_FILE
+echo 'Changing bridge file' >> $LOG_FILE
 network-bridge=/etc/sysconfig/network-scripts/ifcfg-$bridge
 sudo $network-bridge > 'TYPE=OVSPort'
 sudo $network-bridge >> 'NAME=$bridge'
@@ -70,8 +70,8 @@ sudo $network-bridge >> 'DEVICE=$bridge'
 sudo $network-bridge >> 'DEVICETYPE=ovs'
 sudo $network-bridge >> 'OVS_BRIDGE=br-ex'
 sudo $network-bridge >> 'ONBOOT=yes'
-cat $network-bridge >> LOG_FILE
-service network restart >> LOG_FILE
+cat $network-bridge >> $LOG_FILE
+service network restart >> $LOG_FILE
 
 echo "Note:  Log File:  /var/log/openstack_automate.log"
-echo "INSTALLATION HAS BEEN COMPLETED. Please check keystonerc_admin file in userdirectory " >> LOG_FILE
+echo "INSTALLATION HAS BEEN COMPLETED. Please check keystonerc_admin file in userdirectory " >> $LOG_FILE
